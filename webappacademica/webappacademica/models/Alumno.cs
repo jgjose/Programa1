5de7e9d11d5334a2722248980a19b92b9dffe505
jgjose <jg4553571@gmail.com>
2025-10-28 15:52:21 -0700

    continuacion del trabajo en clase

---
+++ webappacademica/webappacademica/models/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webappacademica.models
+{
+    public class Alumno
+    {
+        [Key]
+        public int idAlumno { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Dirrecion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
---
+++ webappacademica/webappacademica/models/MyDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace webappacademica.models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class MyDbContext : DbContext {
+    public MyDbContext() { }
+
+    public MyDbContext(DbContextOptions<MyDbContext> option) : base(option) { }
+    public DbSet<Alumno>Alumnos { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity <Alumno>().HasKey(a => a.idAlumno);
+    }
+
+
+
+}
+}
+
+    
+
